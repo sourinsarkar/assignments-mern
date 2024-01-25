0fd480e41902aa6995557b7017d4618d8bef82ad
Sourin <iamsourinsarkar@gmail.com>
2024-01-26 01:17:45 +0530

    Minor changes

--- week-1/offline-class-1/level-2/02-Date.js
+++ week-1/offline-class-1/level-2/02-Date.js
@@ -20,7 +20,7 @@ function dateMethods() {
   // Getting and setting time in milliseconds since 1970
   console.log("Time in milliseconds since 1970:", currentDate.getTime());
 
-  // const newDate = new Date(2023, 8, 15); // Creating a new date
+  const newDate = new Date(2023, 8, 15); // Creating a new date
   console.log("New Date:", newDate);
   console.log(newDate.getDate());
 }
