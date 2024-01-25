6a2580be5112fd9114de0432ee10cfd336d4f9fe
Sourin <iamsourinsarkar@gmail.com>
2024-01-26 01:14:56 +0530

    Tested Date and time in JS

--- week-1/offline-class-1/level-2/02-Date.js
+++ week-1/offline-class-1/level-2/02-Date.js
@@ -20,8 +20,9 @@ function dateMethods() {
   // Getting and setting time in milliseconds since 1970
   console.log("Time in milliseconds since 1970:", currentDate.getTime());
 
-  const newDate = new Date(2023, 8, 15); // Creating a new date
+  // const newDate = new Date(2023, 8, 15); // Creating a new date
   console.log("New Date:", newDate);
+  console.log(newDate.getDate());
 }
 
 // Example Usage for Date Methods
