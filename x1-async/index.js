c3adb20dd3cc8767a752ea4dcf70ce87b3e371b4
Sourin <iamsourinsarkar@gmail.com>
2024-02-18 22:45:27 +0530

    Practised Promises

---
+++ x1-async/index.js
@@ -0,0 +1,12 @@
+let d = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Something");
+    }, 2000)
+})
+
+function callback() {
+    console.log(d);
+}
+
+console.log(d) // Promises can be used outside the functions too. It is just like other classes. It prints Promise as pending because the promise is getting resolved after 2 minutes.
+d.then(callback); // This is throws the string that has been passed to the promise because it is being resolved and then printed.
