e57a7f828fe30ba3de01dfda67fafcbbe8a14b1e
Sourin <iamsourinsarkar@gmail.com>
2024-02-18 22:30:16 +0530

    Revising Callbacks and Promise

--- x/index.js
+++ x/index.js
@@ -1,7 +1,50 @@
 const fs = require("fs")
 
-fs.readFile("a.txt", "utf-8", (err, data) => {
-    console.log(data);
-})
+// fs.readFile("a.txt", "utf-8", (err, data) => {
+//     if(err) console.log(err);
+//     else console.log(data);
+// });
 
-console.log("Hi there")
+// console.log("Hi there")
+
+// let a = 0;
+// for(let i = 0; i < 10000000; i++) {
+//     a++;
+// }
+
+// console.log("Hi for the second time.")
+
+// -----------------------------------------------------
+
+// Code with CALLBACKS
+
+function toRead(data) {
+    console.log(data);
+}
+
+function toPrint(callBack) {
+    console.log("Hello from CALLBACK.")
+    fs.readFile("a.txt", "utf-8", (err, data) => {
+        console.log("Check for callback")
+        callBack(data);
+    });
+}
+
+toPrint(toRead);
+
+// -----------------------------------------------------
+
+// Code with PROMISES
+
+function toPrint2() {
+    console.log("Hello from PROMISE.");
+    return new Promise(function(resolve) {
+        fs.readFile("a.txt", "utf-8", (err, data) => {
+            console.log("Check for promise")
+            resolve(data);
+        });
+    });
+}
+
+toPrint2()
+.then(toRead)
